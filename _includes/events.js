6f089bfb560212c0d73f003d54827db6ac8f69c9
Neeraj Chekutty <64561854+NeerajC05@users.noreply.github.com>
2022-07-10 23:32:22 +0100

    Initial commit (DONE - Events and Join us)

---
+++ _includes/events.js
@@ -0,0 +1,7 @@
+{
+    name: "Weekly Meeting",
+    description: "6-8pm BST - Room 11 (Lower Ground), 40 George Square, Edinburgh, EH8 9JX. Please use the Automatic doors at the side entrance on 40 George Square.",
+    date: ["September/22/2022"],        
+    type: "event",
+    color: "#1e90ff"
+}
